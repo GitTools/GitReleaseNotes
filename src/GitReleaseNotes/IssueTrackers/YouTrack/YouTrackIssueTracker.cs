6cfebe98e768ba87bf0b4b21b9a62e5fe1e6b45f
Petrik van der Velde <petrikvandervelde@gmail.com>
2014-04-30 15:03:18 +1200

    Adding the YouTrackIssueTracker class and the necessary settings.
    references #17

---
+++ src/GitReleaseNotes/IssueTrackers/YouTrack/YouTrackIssueTracker.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace GitReleaseNotes.IssueTrackers.YouTrack
+{
+    public sealed class YouTrackIssueTracker : IIssueTracker
+    {
+        private readonly GitReleaseNotesArguments _arguments;
+        private readonly Regex _issueNumberRegex;
+        private readonly IYouTrackApi _youTrackApi;
+
+        public YouTrackIssueTracker(IYouTrackApi youTrackApi, GitReleaseNotesArguments arguments)
+        {
+            _youTrackApi = youTrackApi;
+            _arguments = arguments;
+            _issueNumberRegex = new Regex(string.Format(@"(?<issueNumber>{0}-\d+)", arguments.JiraProjectId));
+        }
+
+        public bool VerifyArgumentsAndWriteErrorsToConsole()
+        {
+            if (string.IsNullOrEmpty(_arguments.YouTrackServer) ||
+                !Uri.IsWellFormedUriString(_arguments.YouTrackServer, UriKind.Absolute))
+            {
+                Console.WriteLine("A valid YouTrack server must be specified [/YouTrackServer ]");
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(_arguments.YouTrackProjectId))
+            {
+                Console.WriteLine("/YouTrackProjectId is a required parameter for YouTrack");
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(_arguments.Username))
+            {
+                Console.WriteLine("/Username is a required to authenticate with YouTrack");
+                return false;
+            }
+            if (string.IsNullOrEmpty(_arguments.Password))
+            {
+                Console.WriteLine("/Password is a required to authenticate with YouTrack");
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(_arguments.YouTrackFilter))
+            {
+                _arguments.YouTrackFilter = string.Format("project = {0} AND " +
+                               "(issuetype = Bug OR issuetype = Story OR issuetype = \"New Feature\") AND " +
+                               "status in (Closed, Done, Resolved)", _arguments.YouTrackProjectId);
+            }
+
+            return true;
+        }
+
+        public void PublishRelease(string releaseNotesOutput)
+        {
+            Console.WriteLine("YouTrack does not support publishing releases");
+        }
+
+        public IEnumerable<OnlineIssue> GetClosedIssues(DateTimeOffset? since)
+        {
+            return _youTrackApi.GetClosedIssues(_arguments, since).ToArray();
+        }
+
+        public Regex IssueNumberRegex
+        {
+            get
+            {
+                return _issueNumberRegex;
+            }
+        }
+
+        public bool RemotePresentWhichMatches
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
