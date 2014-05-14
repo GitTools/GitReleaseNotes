0c008a90c6132731e40d83206ee4a9b2c78e1c93
Petrik van der Velde <petrikvandervelde@gmail.com>
2014-05-15 07:35:43 +1200

    Making sure that each release always has an end date. Currently using the start date from the starting tag.
    references #28

--- src/GitReleaseNotes/CommitGrouper.cs
+++ src/GitReleaseNotes/CommitGrouper.cs
@@ -20,7 +20,7 @@ namespace GitReleaseNotes
                     var tag = tagLookup[commit.Sha];
                     var releaseDate = ((Commit) tag.Target).Author.When;
                     currentRelease.Item1.PreviousReleaseDate = releaseDate;
-                    var releaseInfo = new ReleaseInfo(tag.Name, releaseDate, null, commit.Sha);
+                    var releaseInfo = new ReleaseInfo(tag.Name, releaseDate, tagToStartFrom.Commit.Author.When, commit.Sha);
                     var commits = new List<Commit>();
                     currentRelease = new Tuple<ReleaseInfo, List<Commit>>(releaseInfo, commits);
                     releases.Add(releaseInfo, commits);
