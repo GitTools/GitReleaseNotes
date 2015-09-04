5d89510255ea2af873c7384c010057ceaedd089d
Ben Scott <ben@belfryimages.com.au>
2015-09-04 19:24:18 +1000

    #88 Make NuGet and Choco nuspec paths point to the correct repository

--- Chocolatey/tools/chocolateyInstall.ps1
+++ Chocolatey/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName = 'GitReleaseNotes'
 $version = '__version__'
-$url = 'https://github.com/JakeGinnivan/GitReleaseNotes/releases/download/__version__/GitReleaseNotes.__version__.zip'
+$url = 'https://github.com/GitTools/GitReleaseNotes/releases/download/__version__/GitReleaseNotes.__version__.zip'
 $validExitCodes = @(0) 
 
 Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
