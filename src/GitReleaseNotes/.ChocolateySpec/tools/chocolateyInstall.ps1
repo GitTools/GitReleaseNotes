88d741b6f390201a3f50f88b0b7205285df0e6a8
Jake Ginnivan <jake@ginnivan.net>
2015-09-16 17:02:15 +0800

    Fixing comments from chocolatey package review

--- src/GitReleaseNotes/.ChocolateySpec/tools/chocolateyInstall.ps1
+++ src/GitReleaseNotes/.ChocolateySpec/tools/chocolateyInstall.ps1
@@ -1,6 +1,1 @@
-$packageName = 'GitReleaseNotes'
-$version = '__version__'
-$url = 'https://github.com/GitTools/GitReleaseNotes/releases/download/__version__/GitReleaseNotes.__version__.zip'
-$validExitCodes = @(0) 
-
-Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+Generate-BinFile "grn" "$packageFolder\Tools\GitReleaseNotes.exe"
